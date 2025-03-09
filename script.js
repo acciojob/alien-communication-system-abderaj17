28e2a9c6c91171dc4088252fb1ec6469c3d0df2a
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-09 13:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+console.log("Start");
+
+setTimeout(function () {
+	console.log("setTimeout");
+},0);
+
+Promise.resolve().then(function () {
+	console.log("Promise");
+});
+
+console.log("End");
